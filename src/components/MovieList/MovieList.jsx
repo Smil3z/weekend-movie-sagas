f5d0abfa7370c4938e737f67ff1b1866367be023
Stephon Tonge <stephon.tonge75@gmail.com>
2024-01-15 16:57:07 -0600

    added selectedMovie to storeJS

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -11,15 +11,23 @@ function MovieList() {
     dispatch({ type: 'FETCH_MOVIES' });
   }, []);
 
+  const displayMovie = (movieToDisplay) => {
+    console.log(movieToDisplay);
+    // dispatch sends data to redux and sagas
+    dispatch({type: 'SET_MOVIE_DETAILS', payload: movieToDisplay})
+  }
+
   return (
     <main>
       <h1>MovieList</h1>
       <section className="movies">
+        {/* Movies is an array */}
         {movies.map(movie => {
+          // For each movie in the array, Display it on the DOM
           return (
             <div data-testid='movieItem' key={movie.id}>
               <h3>{movie.title}</h3>
-              <img src={movie.poster} alt={movie.title}/>
+              <img onClick={() => displayMovie(movie)} src={movie.poster} alt={movie.title}/>
             </div>
           );
         })}
